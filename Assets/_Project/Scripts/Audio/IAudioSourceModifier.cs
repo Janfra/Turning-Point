c7f3f85dbf047184cbbe2f217d287c8ee6806eb7
Jan <janfrancoherrera@gmail.com>
2025-11-01 14:26:56 +0000

    Audio Package
    # Description
    Audio scripts have been transferred to the `Janito-Unity-Packages` and converted into a Unity package format. Now they are included as a package.
    
    Fixed references lost during transition in `UnityEvents`.

--- Assets/_Project/Scripts/Audio/IAudioSourceModifier.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Janito.Audio
-{
-    public interface IAudioSourceModifier
-    {
-        public abstract void ApplyAudioSourceModification(AudioSource audioSource);
-    }
-}
